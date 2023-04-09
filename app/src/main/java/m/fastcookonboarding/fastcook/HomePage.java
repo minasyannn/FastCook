a17edc0145389924b3d64278630999ca2d47c9f6
Gagik <gagik.krmoyan@gmail.com>
2023-04-09 13:17:57 +0400

    Commit

---
+++ app/src/main/java/m/fastcookonboarding/fastcook/HomePage.java
@@ -0,0 +1,23 @@
+package m.fastcookonboarding.fastcook;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomePage extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity);
+
+    }
+
+}
+
+
